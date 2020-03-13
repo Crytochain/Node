a32cb8b1ba7ac75baa9adbac5b6f02e4f48f405c
liberumChain <59084371+liberumChain@users.noreply.github.com>
2020-03-13 23:29:31 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-import Button from './button.component'
-module.exports = Button
+// Reusable Dropdown Components
+// TODO: Refactor into separate components
+const Dropdown = require('./components/dropdown').Dropdown
+
+// App-Specific Instances
+const NetworkDropdown = require('./network-dropdown').default
+
+module.exports = {
+  NetworkDropdown,
+  Dropdown,
+}
